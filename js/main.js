25beaa961aa6d74ed28aae753506b2787eb6a6b8
alamin <alaminhosen44000@gmail.com>
2023-09-05 16:29:21 +0600

    c

--- js/main.js
+++ js/main.js
@@ -23,6 +23,38 @@
       $(".offcanvas-overlay").removeClass("active");
     }
   });
+
+  // sticky-menu
+  var wind = $(window);
+  var sticky = $("#sticky-header");
+  wind.on("scroll", function () {
+    var scroll = wind.scrollTop();
+    if (scroll < 5) {
+      sticky.removeClass("sticky");
+    } else {
+      sticky.addClass("sticky");
+    }
+  });
+  $(window).on("load resize", function () {
+    $(".header-section").height($(".header-in").outerHeight());
+    $("body").css("--header-height", $(".header-in").outerHeight() + "px");
+  });
+
+  //one-page-nav
+  var top_offset = $(".header-area").height() - 10;
+  $(".main-menu nav ul").onePageNav({
+    currentClass: "active",
+    scrollOffset: top_offset,
+  });
+
+  $(".page-menu").onePageNav({
+    currentClass: "active",
+    scrollOffset: 0,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: "",
+    easing: "swing",
+  });
 })(jQuery);
 
 $(document).ready(function () {
